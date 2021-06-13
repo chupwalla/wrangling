52f478c2cda1ddd883cdb527f447049cf8c4eb00
Graeme Fickling <chupwalla@gmail.com>
2021-06-13 14:15:09 -0400

    Starting work on Assessing and Cleaning data

--- image_predictions.py
+++ image_predictions.py
@@ -1,7 +1,6 @@
 '''
 Download image-predictions TSV file from the Udacity servers using the Requests library
 '''
-import csv
 import requests
 
 r = requests.get('https://d17h27t6h515a5.cloudfront.net/topher/2017/August/599fd2ad_image-predictions/image-predictions.tsv')
